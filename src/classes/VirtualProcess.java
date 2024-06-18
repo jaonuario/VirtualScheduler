728e901b0c0cf184f621b538a8c7066fa08f9289
muro <jpedrojanus@hotmail.com>
2024-06-18 11:41:37 -0300

    Add methods in VirtualProcess

--- src/classes/VirtualProcess.java
+++ src/classes/VirtualProcess.java
@@ -1,5 +1,45 @@
 package classes;
 
 public class VirtualProcess {
+    private static int burstValue = 0;
+    private Integer[] seqc;
+    private int counter;
+
+    public VirtualProcess(String name, Integer[] seqc){
+        this.counter = 0;
+        this.seqc = seqc;
+    }
+
+    public void run(){
+
+    }
+    
+    private void execute(){
         
+    }
+
+    private void block(int value){
+        
+    }
+
+    private void nextCommand(){
+        if(!hasNextCommand()){
+            return;
+        }
+
+        if(seqc[counter] == 0){
+            execute();
+        }
+        else{
+            block(seqc[counter]);
+        }
+
+        counter++;
+    }   
+
+    private boolean hasNextCommand(){
+        if(counter < seqc.length)
+            return true;
+        return false;
+    }
 }
