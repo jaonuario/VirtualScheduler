c062ab682c0d3b31c230963c8a057fbd3a4bde46
João Januario <jpedrojanus@hotmail.com>
2024-06-22 14:40:22 -0300

    virtual process implementation

--- src/classes/VirtualProcess.java
+++ src/classes/VirtualProcess.java
@@ -1,45 +1,75 @@
 package classes;
 
 public class VirtualProcess {
-    private static int burstValue = 0;
+    //Constantes
+    public static final long BURST_VALUE = 4000;
+    public static final short READY = 0;
+    public static final short EXEC = 1;
+    public static final short BLOCK = 2;
+
+    private String name;
     private Integer[] seqc;
+    private long birth;
+    private int status;
     private int counter;
 
     public VirtualProcess(String name, Integer[] seqc){
-        this.counter = 0;
+        this.name = name;
         this.seqc = seqc;
+        this.birth = System.currentTimeMillis();
+        this.counter = 0;
+        this.status = READY;
     }
 
-    public void run(){
+    public int run(){
+        while (hasNextCommand()) {
+            nextCommand();
+        }
+        return status;
+    }
 
+    public long getAge(){
+        return System.currentTimeMillis() - birth;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getStatus(){
+        return this.status;
     }
     
     private void execute(){
-        
+        burst();
     }
 
     private void block(int value){
-        
+        burst();
+        this.status = BLOCK;
     }
 
-    private void nextCommand(){
-        if(!hasNextCommand()){
-            return;
-        }
-
+    public void nextCommand(){
         if(seqc[counter] == 0){
             execute();
         }
         else{
             block(seqc[counter]);
         }
-
         counter++;
     }   
 
-    private boolean hasNextCommand(){
+    public boolean hasNextCommand(){
         if(counter < seqc.length)
             return true;
         return false;
+    }
+
+    private void burst(){
+        long start = System.currentTimeMillis();
+        long current;
+        do {
+            current = System.currentTimeMillis();
+        } while ((current - start) < BURST_VALUE);
     }
 }
